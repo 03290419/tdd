2deda39865f5b6bd35394aa1f17200582849d732
pguswls <alfma123@naver.com>
2023-03-02 20:07:56 +0900

    feat: first tdd

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div``;
+const Title = styled.div``;
+
+export const PageHeader = () => {
+  return (
+    <Container>
+      <Title>할 일 목록</Title>
+    </Container>
+  );
+};
